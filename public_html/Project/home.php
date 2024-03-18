595faf1a6b0c3bf880319fee6f27050ecce12139
TiaraJenks <tjenkinsamaker@gmail.com>
2024-03-18 17:00:36 -0400

    User Profile

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -3,13 +3,13 @@ require(__DIR__."/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-/*if(is_logged_in()){
+if(is_logged_in()){
     flash("Welcome, " . get_user_email());
 }
 else{
     flash("You're not logged in");
 }
-if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+/*if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
  echo "Welcome, " . $_SESSION["user"]["email"]; 
 }
 else{
